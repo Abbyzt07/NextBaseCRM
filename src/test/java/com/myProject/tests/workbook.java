1f7b6d584d75faf22a1990cc19b0188c46e6042f
Yavuz Kaya <yavuzkaya3866@gmail.com>
2020-10-21 10:21:54 +0200

    commit 21 october

--- src/test/java/com/myProject/tests/workbook.java
+++ src/test/java/com/myProject/tests/workbook.java
@@ -3,5 +3,6 @@ package com.myProject.tests;
 public class workbook {
     public static void main(String[] args) {
         System.out.println("Hello yavuz");
+        System.out.println("21-10-2020");
     }
 }
